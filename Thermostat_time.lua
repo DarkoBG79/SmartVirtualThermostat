020d257eef1a33364f6bc26a5787025b36a2bd51
999LV <lv999@online.fr>
2017-01-17 15:54:54 +0100

    Version 0.1
    First beta release

---
+++ Thermostat_time.lua
@@ -0,0 +1,341 @@
+-- Smart Virtual Thermostat for Domoticz, by logread
+--[[
+Version 0.1 Jan 16 2017
+Developed by logread, based on the Vera plugin from Antor, but significantly rewritten due to Domoticz peculiarities
+
+Installation: see doc on ???
+--]]
+
+commandArray = {}
+
+--###################### BEGINNING OF USER PARAMETERS #####################
+
+-- system parameters for automated uservariables creation via Domoticz json API
+-- MUST BE VALID for the relevant system
+local ip = "127.0.0.1" -- make sure Domoticz allows password free call on that IP and does not block it
+local port = "8080" -- the local port on which to access Domoticz
+
+-- Domoticz devices used by script.
+-- virtual devices MUST be manually created before running script)
+-- single devices
+  local thermostat = "Thermostat" -- virtual, type switch
+  local forcedheating = "Forced_Heating" -- virtual, type switch
+  local tempthermostat = "Temp_Thermostat" -- virtual, type temperature
+-- tables of device(s) allowing multiple sensors and heaters device(s)
+  local setpoints = {"Day_SetPoint", "Night_SetPoint"} -- virtual type thermostat setpoint. Need BOTH !
+  local heaters = {"Heater"} -- can obviously be virtual or physical
+  local sensorsin = {"Temp_Inside"}
+  local sensorsout = {"Temp_Outside"}
+  local sensorspause = {"Door"}
+-- end of list of devices to be adjusted / created by user
+
+-- scripts constants - can be modified by user
+local dirtydata = 3600 -- number of seconds since last update, used to determine if a given sensor is alive or presumed dead
+local powermin = 0 -- minimum heating at each calculation cycle (0-100)
+local deltamax = 0.2 -- allowed temp excess over setpoint temperature
+local debug = true -- turns on/off logging for debugging purposes
+
+-- ###################### END OF USER PARAMETERS #####################
+
+-- Domoticz "user variables" used by script
+-- Will be created automatically by running script
+local uv = {
+  Internals = {name = "SVT_Internals", type = 2, default = "0,0,0,0,0,0,60,1"},
+  DayStartHour = {name = "SVT_DayStartHour", type = 4, default = "07:00"},
+  NightStartHour = {name = "SVT_NightStartHour", type = 4, default = "22:00"},
+  CalcInterval = {name = "SVT_CalculationInterval", type = 0, default = 1200}, -- 20 minutes
+  ForcedHeatingDuration = {name = "SVT_ForcedHeatingDuration", type = 0, default = 3600}, -- 1 hour
+  PauseDelay = {name = "SVT_PauseDelay", type = 0, default = 60}, -- delay set to 60 seconds for instance
+  AutoLearning = {name = "SVT_AutoLearning", type = 2, default = "0,1,1,0,0,0"}
+  }
+-- end of Domoticz user variables created by script
+
+-- script constants or variables, not for user modification
+local data = {} -- Table that holds the runtime variables (filled by GetData function)
+local datafields = {"LastCalc", "EndHeatTime", "IsPaused","Heating","ForcedHeating","ForcedHeatingTime","ConstC","ConstT"}
+local intemp = 20
+local outtemp = 0
+local setpoint = 20
+local pause, pausechanged
+local AutoLearning
+
+-- UTILITY FUNCTIONS
+
+-- converts a Domoticz date string to a Unix epoch
+local function datetoepoch(datestring)
+	local template = "(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)"
+	local year, month, day, hour, minutes, seconds = datestring:match(template)
+	return os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+end
+
+local function CreateUserVariable(varname, vartype, value)
+  --vartype: 0 = Integer, 1 = Float, 2 = String, 3 = Date, 4 = Time, 5 = DateTime
+  value = tostring(value)
+  local url = "http://%s:%s/json.htm?type=command&param=saveuservariable&vname=%s&vtype=%d&vvalue=%s"
+  table.insert(commandArray, { ["OpenURL"] = string.format(url, ip, port, varname, vartype, value)})
+end
+
+local function variable_get(variable)
+  local value = uservariables[variable.name]
+  if value then
+    return value
+  else
+    print("SVT: creating user variable " .. variable.name) 
+    CreateUserVariable(variable.name, variable.type, variable.default)
+    return variable.default
+  end
+end
+
+local function variable_set(variable, value)
+  if value ~= variable_get(variable) then
+    local tempstr = "Variable:" .. variable.name
+    commandArray[tempstr] = tostring(value)
+    return true
+  else
+    return false -- no change
+  end
+end
+
+local function debuglog(message)
+  if not(debug) then return end
+  message = tostring(message) or ""
+  print("SVT Debug: " .. message)
+end
+
+local function DebugTable(name, table)
+  if not(debug) then return end
+  for index, value in pairs(table) do
+    if type(value) == "table" then
+      DebugTable(index, value)
+    else
+      debuglog("table " .. name .. "[" .. index .. "]=" .. tostring(value))
+    end
+  end
+end
+
+local function TableToNumbersString(t)
+  t = t or {}
+  return table.concat(t, ",")
+end
+
+local function NumbersStringToTable(tempstr)
+  tempstr = tempstr or ""
+  local t = {}
+  for v in string.gmatch(tempstr, "(-?[0-9.]+)") do
+    table.insert(t, tonumber(v))
+  end
+  return t
+end
+
+local function round(num, idp)
+  local mult = 10^(idp or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
+-- THERMOSTAT FUNCTIONS
+
+local function GetData()
+  local t = NumbersStringToTable(variable_get(uv.Internals))
+  for index, key in ipairs(datafields) do data[key] = t[index] end
+  AutoLearning = NumbersStringToTable(variable_get(uv.AutoLearning))
+end
+
+local function SaveData()
+  local t = {}
+  for index, key in ipairs(datafields) do t[index] = data[key] end
+  variable_set(uv.Internals, TableToNumbersString(t))
+  variable_set(uv.AutoLearning, TableToNumbersString(AutoLearning))
+end
+
+local function GetTemp(sensors)
+  local nbsensors = 0 
+  local sumtemps = 0
+  for _, device in pairs(sensors) do
+    if datetoepoch(otherdevices_lastupdate[device]) - os.time() <= dirtydata then
+      nbsensors = nbsensors + 1
+      sumtemps = sumtemps + tonumber(otherdevices[device])
+    end
+  end
+  if nbsensors > 0 then
+    return math.floor(sumtemps / nbsensors * 10 + 0.5) /10
+  else
+    return nil
+  end
+end
+
+local function GetPause()
+  local ispause = 0
+  local trigger = 0
+  local lastupdate = 0
+  for _, device in pairs(sensorspause) do
+    if otherdevices[device] == "Open" or otherdevices[device] == "On" then trigger = trigger + 1 end
+    lastupdate = math.max(lastupdate, datetoepoch(otherdevices_lastupdate[device]))
+  end
+  local delai = (os.time() - lastupdate) >= variable_get(uv.PauseDelay)
+  if trigger >= 1 and delai then ispause = 1 end
+  if trigger == 0 and not(delai) then ispause = 1 end -- sensors are off but not for long enough
+  local changed = ispause ~= data.IsPaused
+  data.IsPaused = ispause
+  return ispause ~= 0, changed
+end
+
+local function GetSetPoint()
+  local starthour = string.gsub(variable_get(uv.DayStartHour), ":", "") or "0700"
+  starthour = tonumber(starthour)
+  local endhour = string.gsub(variable_get(uv.NightStartHour), ":", "") or "2200"
+  endhour = tonumber(endhour)
+  local nowtime = os.date("*t", os.time())
+  local nowhour = nowtime.hour * 100 + nowtime.min
+  if (nowhour >= starthour) and (nowhour <= endhour) then
+      return tonumber(otherdevices[setpoints[1]]) or 20 -- this is day setpoint
+  else
+      return tonumber(otherdevices[setpoints[2]]) or 19 -- this is night setpoint
+  end
+end
+
+local function Heat(action)
+  action = action or "Off"
+  for _, device in pairs(heaters) do
+    if otherdevices[device] ~= action then commandArray[device] = action end
+  end
+  if action == "On" then
+    data.Heating = 1
+  else
+    data.Heating = 0
+    data.EndHeatTime = 0
+    if otherdevices[forcedheating] ~= "Off" then commandArray[forcedheating] = "Off" end
+    data.ForcedHeating = 0 --variable_set(uv.ForcedHeating, 0)
+  end
+  SaveData()
+end
+
+local function AutoCallib()
+  local now = os.time()
+  if #AutoLearning ~= 6 then
+    AutoLearning = NumbersStringToTable("0,1,1,0,0,0")
+    return true
+  elseif AutoLearning[1] ~= 1 then
+    -- data not initilised
+    return false
+  elseif AutoLearning[6] == 0 then
+    -- Heater was off, nothing to learn
+    return false
+  elseif AutoLearning[6] == 100 and setpoint > intemp then
+    -- Heater was on max but consigne was not reached so we dont learn
+    return false
+  elseif intemp > AutoLearning[4] and setpoint > AutoLearning[4]  then
+    -- Learn ConstC
+    debuglog("Learning ConstC...")
+    local ConstC = data.ConstC * ((setpoint - AutoLearning[4]) / (intemp - AutoLearning[4] )) * (now - data.LastCalc) / variable_get(uv.CalcInterval)
+    data.ConstC = round((data.ConstC * AutoLearning[2] + ConstC) / (AutoLearning[2] +1),1)
+    debuglog("ConstC = " .. data.ConstC)
+    AutoLearning[2] = math.min(AutoLearning[2] +1 , 50)
+    return true
+  elseif setpoint > AutoLearning[5] then
+    -- Learn ConstT
+    debuglog("Learning ConstT...")
+    local ConstT = data.ConstT + (( setpoint - intemp) / ( setpoint - AutoLearning[5] )) * data.ConstC * (now - data.LastCalc) / variable_get(uv.CalcInterval)
+    data.ConstT = round((data.ConstT * AutoLearning[3] + ConstT) / (AutoLearning[3] +1),1)
+    if data.ConstT  < 0 then data.ConstT = 0 end
+    debuglog("ConstT = " .. data.ConstT)
+    AutoLearning[3] = math.min(AutoLearning[3] +1 , 50)
+    return true
+  else
+    return false
+  end
+end
+ 
+local function AutoChangeOver(learn)
+  local now = os.time()
+  if learn~=nil and learn == true then AutoCallib() end
+  local power = round((setpoint - intemp) * data.ConstC + (setpoint - outtemp) * data.ConstT,1)
+  if (power < 0) then power = 0 end -- Limite basse
+  if (power > 100) then power = 100 end -- Limite haute
+  if (power > 0) and (power <= powermin) then power = powermin end -- Seuil mini de power
+  local heatduration = power * (variable_get(uv.CalcInterval)/100)
+  heatduration = math.floor(heatduration)
+  if power == 0 then
+    data.Heating = 0
+    data.EndHeatTime = 0
+  else
+    data.Heating = 1
+    data.EndHeatTime = now + heatduration
+  end
+  data.LastCalc = now
+--  variable_set(uv.LastCalc, data.LastCalc)
+  if AutoLearning[1] < 2 then
+    AutoLearning[1] = 1
+    AutoLearning[4] = intemp
+    AutoLearning[5] = outtemp
+    AutoLearning[6] = power
+  end
+  debuglog("power = " .. power)
+  debuglog("heatduration = " .. heatduration)
+end
+
+local function UpdateStatus()
+  intemp = GetTemp(sensorsin)
+  outtemp = GetTemp(sensorsout)
+  if intemp then
+    if tonumber(otherdevices[tempthermostat]) ~= intemp then
+      commandArray['UpdateDevice'] = otherdevices_idx[tempthermostat] .. '|0|' .. intemp
+    end
+    GetData()
+    -- test function
+    if otherdevices[thermostat] == "On" then
+      pause, pausechanged = GetPause()
+      setpoint = GetSetPoint()
+      local now = os.time()
+      if otherdevices[forcedheating] == "On" then -- we deal with forced heating situation
+        if otherdevices_lastupdate[forcedheating] > data.ForcedHeatingTime then -- newly set
+          data.ForcedHeatingTime = now
+          data.ForcedHeating = 1
+          data.EndHeatTime = now + variable_get(uv.ForcedHeatingDuration)
+          data.Heating = 1
+        end
+      elseif pause then -- we deal with a pause situation
+        data.Heating = 0
+      elseif not(pause) and pausechanged then -- the pause just stopped... reset normal situation but no learning
+        AutoChangeOver(false)
+      else -- automatic mode
+        if data.LastCalc <= now - variable_get(uv.CalcInterval) then
+          debuglog("Calculation !")
+          AutoChangeOver(true)
+        elseif intemp >= setpoint + deltamax then
+          debuglog("Temp exceeds setpoint... no heating")
+          data.Heating = 0 -- AutoChangeOver(true)
+--          data.LastCalc = now
+        end
+      end
+      -- the actual "heat or no heat" question
+      if data.Heating == 1 and now <= data.EndHeatTime then
+        Heat("On")
+      else
+        Heat("Off")
+      end
+    else 
+      -- thermostat is off but turn off heaters only if last update of any heater was later than thermostat shutdown
+      -- so that user can force a heater on despite thermostat being off
+      local lastupdate = 0
+      for _, device in pairs(heaters) do
+        lastupdate = math.max(lastupdate, datetoepoch(otherdevices_lastupdate[device]))
+      end
+      if datetoepoch(otherdevices_lastupdate[thermostat]) > lastupdate then Heat("Off") end
+      -- turn off forcedheating device if it was still on despite thermostat being off
+      if otherdevices[forcedheating] ~= "Off" then commandArray[forcedheating] = "Off" end
+    end
+  else
+    print("SVT: Error reading temps... Thermostat disabled")
+    Heat("Off")
+    if otherdevices[thermostat] ~= "Off" then commandArray[thermostat] = "Off" end
+  end
+end
+
+-- MAIN PROGRAM BODY
+
+-- code to run every time event (i.e. every minute)
+
+UpdateStatus()
+DebugTable("commandArray", commandArray)
+
+return commandArray
